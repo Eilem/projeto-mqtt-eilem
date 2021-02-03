a6788672d27cd9a40d8d3a20df763d23908d8803
Eilem <eilem.gomes@uotz.com.br>
2021-02-03 09:37:52 -0300

    inserindo arquivos com códigos das aulas

---
+++ ipArduino.ino
@@ -0,0 +1,39 @@
+#include <UIPEthernet.h>
+#include <utility/logging.h>
+
+#include <SPI.h>
+
+
+//Define o endereço MAC que será utilizado
+byte mac[] = {0x44, 0x6E, 0x0F, 0x7F, 0xA4, 0xFE};
+
+//Inicia o cliente Ethernet
+EthernetClient client;
+
+void setup() {
+    //Inicia o controlador Ethernet e solicita um IP para o servidor de DHCP
+    Ethernet.begin(mac);
+
+    //Inicia o monitor Serial
+    Serial.begin(9600);
+
+    //Exibe no Monitor Serial as informações sobre o IP do Arduino
+    Serial.print("O IP do Arduino e: ");
+    Serial.println(Ethernet.localIP());
+
+    //Exibe no Monitor Serial as informações sobre a Máscara de Rede do Arduino
+    Serial.print("A Mascara de Rede do Arduino e: ");
+    Serial.println(Ethernet.subnetMask());
+
+    //Exibe no Monitor Serial as informações sobre o Gateway do Arduino
+    Serial.print("O Gateway do Arduino e: ");
+    Serial.println(Ethernet.gatewayIP());
+
+    //Exibe uma linha em branco
+    Serial.println("");
+
+}
+
+void loop() {
+
+}
